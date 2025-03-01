f74c2e145122e7bb39804b23bce938eeea9e6f00
denki307 <mbtdenki@gmail.com>
2025-03-01 14:24:51 +0530

    Update start.py

--- VIPMUSIC/utils/inline/start.py
+++ VIPMUSIC/utils/inline/start.py
@@ -39,7 +39,7 @@ def private_panel(_):
         [
              InlineKeyboardButton(
                 text="❍ 𝐎ᴡɴᴇʀ ❍",
-                url=f"https://t.me/OwnerofTheDenki",
+                url=f"https://t.me/Murugan_008",
             ),
             InlineKeyboardButton(
                 text="❍ 𝐀ʟʟ 𝐁ᴏᴛs ❍",
@@ -49,7 +49,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text="❍ 𝐏𝐑𝐎𝐌𝐎𝐓𝐈𝐎𝐍 𝐀𝐕𝐀𝐈𝐋𝐀𝐁𝐋𝐄 ❍",
-                url=f"https://t.me/OwnerofTheDenki",
+                url=f"https://t.me/Murugan_008",
             ),
         ],
     ]
