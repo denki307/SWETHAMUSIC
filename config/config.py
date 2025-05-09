e30e2593e982d0d0e43aeb3bdb075bdead638c2b
denki307 <mbtdenki@gmail.com>
2025-05-09 12:30:55 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -52,7 +52,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/denki307/phycho",
+    "https://github.com/denki307/SWETHAMUSIC",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
